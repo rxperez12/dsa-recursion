d36c88132260c879164ee327bba089e7d4d1b063
Ricardo Perez <rxperez12@gmail.com>
2024-07-09 12:28:28 -0700

    Add product function

--- recursion/product/product.ts
+++ recursion/product/product.ts
@@ -3,7 +3,13 @@ import { LLNodeNum } from "../common/ll";
 /** product: calculate the product of a linked list of numbers. */
 
 function product(nums: LLNodeNum | null): number {
-  return 42;
+  // Base case
+  // node.next === null
+  // if we reach the end of list, return node.val
+  if(nums === null) return 1;
+
+  //take first number of list and call rest of list with product funciton
+  return nums.val * product(nums.next)
 }
 
 export { product };
