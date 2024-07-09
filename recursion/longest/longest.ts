b8b10081577fd7c97fc93095f7151fe557367e08
Andrea Jao <summamazing@gmail.com>
2024-07-09 14:42:37 -0700

    longest.ts pseudocode

--- recursion/longest/longest.ts
+++ recursion/longest/longest.ts
@@ -3,6 +3,27 @@ import { LLNodeStr } from "../common/ll";
 /** longest: return length of longest word in a linked list of words. */
 
 function longest(words: LLNodeStr | null): number {
+
+  /**
+   * { val: "apple" , next: { val: "orange", next: { val: "cherry", next: null } } }
+   *
+   * base case: if next === null
+   * regular case:
+   *  - check the length of my val
+   *  - compare it to the length of my next's val
+   *  - check function scope counter and return???
+   * can we update a counter in the function scope??
+   */
+
+  let longestLength = 0
+
+  if (words === null) return 0;
+  if (words.next === null) return 0;
+
+  const currentValLength = words.val.length
+
+
+
   return 42;
 }
 
