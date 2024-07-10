6bda87eba38b90f2a1cb60ad3a4ae3058a70c301
Andrea Jao <summamazing@gmail.com>
2024-07-09 18:09:54 -0700

    completed binarySearch

--- recursion/binary-search/binarySearch.ts
+++ recursion/binary-search/binarySearch.ts
@@ -5,31 +5,32 @@ function binarySearch(arr: number[], val: number, left = 0, right = arr.length):
 
   /**
    * base case = if the function returns true
-   * [1, 2, 3, 4, 5] , 7
-   * [4, 5] , 7
-   * [5], 7
+   * [1, 2, 3, 4 ] , 4
+   * [4, 5] , 4
+   * [5], 4
    * false
    * check a single number (middle number) in the array against val
    * if mid > val, give the right side of array
    * if mid < val, give the left side of the array
    */
 
-  if (arr.length === 0) return false;
+  if (arr.length === 0 || left < 0 || right > arr.length || left > right)
+     return false;
 
-  const midPoint = Math.floor((right-left)/2)
+  const midPoint = Math.floor((right+left)/2)
+  let answer = false
 
   if (arr[midPoint] === val){
-    return true
-
-  } else if (arr[midPoint] > val){
-    binarySearch(arr, val, left = midPoint, right = arr.length)
+    answer = true
 
   } else if (arr[midPoint] < val){
-    binarySearch(arr, val, left = 0, right = midPoint)
+    answer = binarySearch(arr, val, left = midPoint + 1, right = arr.length)
+
+  } else if (arr[midPoint] > val){
+    answer = binarySearch(arr, val, left = 0, right = midPoint - 1)
   }
 
-
-  return false;
+  return answer;
 }
 
 export { binarySearch };
